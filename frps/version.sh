22489a0c633a3d86dc1aefa5bc8f31566f5a52db
SnowMoonSS <snowmoonss@outlook.com>
2020-09-12 14:11:32 +0800

    尝试阿里源

--- frps/version.sh
+++ frps/version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export FRPS_VER=0.33.0
 export FRPS_INIT="https://raw.githubusercontent.com/SnowMoonSS/onekey-install-shell/master/frps/frps.init"
-#export aliyun_download_url="https://code.aliyun.com/clangcn/frp/raw/master"
+export aliyun_download_url="https://codeup.aliyun.com/5f5c52bcd24814603933dcd1/SnowMoonSS/frp/raw/master"
 export github_download_url="https://github.com/fatedier/frp/releases/download"
